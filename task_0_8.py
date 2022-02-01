4b4bfa5a53059b29522f50215d1f6ab490b59811
Titanmash <82869895+Titanmash@users.noreply.github.com>
2022-02-01 18:17:16 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,12 +1,11 @@
 def hours_converter(hour):
-    hours = ' hour, '
+    hours = ' hour '
     minute = ' minute '
-
     x = divmod(hour, 60)
-    if x[0] != 0 and x[0] != 1 and x[0] != 0:
-        hours = " hours, "
+    if x[0] != 1:
+        hours = " hours "
 
-    if x[1] != 0 and x[0] != 1 and x[1] != 1:
+    if x[1] != 1:
         minute = " minutes "
 
     return str(x[0]) + hours + str(x[1]) + minute
