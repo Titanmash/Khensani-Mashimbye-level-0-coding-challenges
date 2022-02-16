fe17b32b148c5d863821c6b1eeaea30625f08a3d
Titanmash <82869895+Titanmash@users.noreply.github.com>
2022-02-16 19:58:04 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -1,22 +1,6 @@
-from collections import Counter
-
-
-def common(str1, str2):
-    dict1 = Counter(str2)
-    dict2 = Counter(str1)
-
-    commonDict = dict1 & dict2
-
-    if len(commonDict) == 0:
-        print(-1)
-
-    commonChars = (commonDict.elements())
-    commonChars = commonChars
-    print("Common letters: " + ', '.join(commonChars))
-
-
-if __name__ == "__main__":
-    str1 = 'house'
-    str2 = 'computers'
-    common(str1, str2)
-
+def common_letters(first_word, second_word):
+    first_word = set(first_word)
+    second_word = set(second_word)
+    print("Common letters:", end=' ')
+    print(','.join(first_word.intersection(second_word)))
+    
