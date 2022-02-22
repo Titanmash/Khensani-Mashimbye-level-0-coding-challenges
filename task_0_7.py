3f9442be096f224a317599988ce45f2e71d4dbff
Titanmash <82869895+Titanmash@users.noreply.github.com>
2022-02-22 09:22:24 +0200

    Update task_0_7.py

--- task_0_7.py
+++ task_0_7.py
@@ -1,7 +1,7 @@
-def temp_celsius(celsius):
-    fahrenheit = int(round((9 * int(celsius)) / 5 + 32))
+def temp_fahrenheit(celsius):
+    fahrenheit = round((9 * celsius) / 5 + 32)
     return fahrenheit
 
-def temp_fahrenheit(fahrenheit):
-    celsius = int(round((fahrenheit - 32) * 5 / 9))
+def temp_celsius(fahrenheit):
+    celsius = round((fahrenheit - 32) * 5 / 9)
     return celsius
